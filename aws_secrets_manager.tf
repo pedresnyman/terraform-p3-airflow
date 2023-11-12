10868e536beecf37b24abded7b5d8983a5c0bb6e
pedre.snyman <pedre.snyman@sendcloud.com>
2023-11-12 15:37:18 +0100

    clean up

--- aws_secrets_manager.tf
+++ aws_secrets_manager.tf
@@ -1,8 +1,17 @@
+resource "random_password" "password" {
+  length           = 16
+  special          = true
+  override_special = "_%@"
+}
+
 resource "aws_secretsmanager_secret" "airflow_metadata_db_secret" {
   name        = "variables/airflow_meta_db"
   description = "Credentials for the Airflow metadata RDS instance"
 }
 
-data "aws_secretsmanager_secret_version" "airflow_metadata_db_secret_version" {
-  secret_id = aws_secretsmanager_secret.airflow_metadata_db_secret.id
+resource "aws_secretsmanager_secret_version" "airflow_metadata_db_secret_version" {
+  secret_id     = aws_secretsmanager_secret.airflow_metadata_db_secret.id
+  secret_string = jsonencode({
+    password = random_password.password.result
+  })
 }
