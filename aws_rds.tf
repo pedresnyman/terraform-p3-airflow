5f752ae4227c614cc4abe1847723785c41444a29
pedre.snyman <pedre.snyman@sendcloud.com>
2024-04-22 22:44:32 +0200

    .

--- aws_rds.tf
+++ aws_rds.tf
@@ -1,7 +1,7 @@
 # A subnet group for our RDS instance.
 resource "aws_db_subnet_group" "airflow_metadata_db" {
   name       = "${var.rds_identifier}-subg"
-  subnet_ids = local.subnet_ids
+  subnet_ids = local.private_subnet_ids
 }
 
 locals {
