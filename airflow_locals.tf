89356abaa6ce6fced34951f83d1b590e38e7a4b6
pedre.snyman <pedre.snyman@sendcloud.com>
2024-09-23 14:47:00 -0400

    airflow

--- airflow_locals.tf
+++ airflow_locals.tf
@@ -72,7 +72,7 @@ locals {
           value = "/airflow-fargate/${key}/"
         },
         {
-          name  = "AIRFLOW__ECS_FARGATE__SECURITY_GROUPS"
+          name  = "AIRFLOW__AWS_ECS_EXECUTOR__SECURITY_GROUPS"
           value = aws_security_group.airflow_fargate_service[key].id
         }
       ])
@@ -91,28 +91,48 @@ locals {
   # airflow environmental variables
   airflow_environment_static_variables = [
     {
-      name  = "AIRFLOW__ECS_FARGATE__REGION"
-      value = var.aws_region
-    },
-    {
-      name  = "AIRFLOW__LOGGING__REMOTE_BASE_LOG_FOLDER"
-      value = "cloudwatch://${aws_cloudwatch_log_group.airflow_logs.arn}:*"
-    },
-    {
-      name  = "AIRFLOW__ECS_FARGATE__SUBNETS"
-      value = join(",", local.private_subnet_ids)
+      name  = "AIRFLOW__CORE__EXECUTOR"
+      value = "airflow.providers.amazon.aws.executors.ecs.ecs_executor.AwsEcsExecutor"
     },
     {
       name  = "AIRFLOW__DATABASE__SQL_ALCHEMY_CONN"
       value = "postgresql+psycopg2://${var.airflow_username}:${local.db_password}@${aws_db_instance.airflow_metadata_db.endpoint}/${var.rds_database_name}"
     },
-    #     {
-    #       name  = "AIRFLOW__WEBSERVER__WARN_DEPLOYMENT_EXPOSURE"
-    #       value = "False"
-    #     },
     {
-      name  = "AIRFLOW__CORE__EXECUTOR"
-      value = "aws_executors_plugin.AwsEcsFargateExecutor"
+      name  = "AIRFLOW__AWS_ECS_EXECUTOR__REGION_NAME"
+      value = var.aws_region
+    },
+    {
+      name  = "AIRFLOW__AWS_ECS_EXECUTOR__CLUSTER"
+      value = "airflow"
+    },
+    {
+      name  = "AIRFLOW__AWS_ECS_EXECUTOR__CONTAINER_NAME"
+      value = "task-executor"
+    },
+    {
+      name  = "AIRFLOW__AWS_ECS_EXECUTOR__TASK_DEFINITION"
+      value = "airflow-task-executor"
+    },
+    {
+      name  = "AIRFLOW__AWS_ECS_EXECUTOR__LAUNCH_TYPE"
+      value = "FARGATE"
+    },
+    {
+      name  = "AIRFLOW__AWS_ECS_EXECUTOR__PLATFORM_VERSION"
+      value = "LATEST"
+    },
+    {
+      name  = "AIRFLOW__AWS_ECS_EXECUTOR__ASSIGN_PUBLIC_IP"
+      value = "DISABLED"
+    },
+    {
+      name  = "AIRFLOW__AWS_ECS_EXECUTOR__SUBNETS"
+      value = join(",", local.private_subnet_ids)
+    },
+    {
+      name  = "AIRFLOW__LOGGING__REMOTE_BASE_LOG_FOLDER"
+      value = "cloudwatch://${aws_cloudwatch_log_group.airflow_logs.arn}:*"
     },
     {
       name  = "AIRFLOW__CORE__PARALLELISM"
@@ -123,30 +143,6 @@ locals {
       value = "100"
     },
     {
-      name  = "AIRFLOW__ECS_FARGATE__CLUSTER"
-      value = "airflow"
-    },
-    {
-      name  = "AIRFLOW__ECS_FARGATE__CONTAINER_NAME"
-      value = "task-executor"
-    },
-    {
-      name  = "AIRFLOW__ECS_FARGATE__TASK_DEFINITION"
-      value = "airflow-task-executor"
-    },
-    {
-      name  = "AIRFLOW__ECS_FARGATE__LAUNCH_TYPE"
-      value = "FARGATE"
-    },
-    {
-      name  = "AIRFLOW__ECS_FARGATE__PLATFORM_VERSION"
-      value = "LATEST"
-    },
-    {
-      name  = "AIRFLOW__ECS_FARGATE__ASSIGN_PUBLIC_IP"
-      value = "DISABLED"
-    },
-    {
       name  = "AIRFLOW__LOGGING__REMOTE_LOGGING"
       value = "True"
     },
@@ -154,7 +150,64 @@ locals {
       name  = "AIRFLOW__LOGGING__REMOTE_LOG_CONN_ID"
       value = "aws_default"
     },
+#     {
+#       name  = "AIRFLOW__ECS_FARGATE__REGION"
+#       value = var.aws_region
+#     },
+#     {
+#       name  = "AIRFLOW__ECS_FARGATE__SUBNETS"
+#       value = join(",", local.private_subnet_ids)
+#     },
+#     {
+#       name  = "AIRFLOW__DATABASE__SQL_ALCHEMY_CONN"
+#       value = "postgresql+psycopg2://${var.airflow_username}:${local.db_password}@${aws_db_instance.airflow_metadata_db.endpoint}/${var.rds_database_name}"
+#     },
+#     {
+#       name  = "AIRFLOW__CORE__EXECUTOR"
+#       value = "aws_executors_plugin.AwsEcsFargateExecutor"
+#     },
+#     {
+#       name  = "AIRFLOW__CORE__PARALLELISM"
+#       value = "100"
+#     },
+#     {
+#       name  = "AIRFLOW__CORE__MAX_ACTIVE_TASKS_PER_DAG"
+#       value = "100"
+#     },
+#     {
+#       name  = "AIRFLOW__ECS_FARGATE__CLUSTER"
+#       value = "airflow"
+#     },
+#     {
+#       name  = "AIRFLOW__ECS_FARGATE__CONTAINER_NAME"
+#       value = "task-executor"
+#     },
+#     {
+#       name  = "AIRFLOW__ECS_FARGATE__TASK_DEFINITION"
+#       value = "airflow-task-executor"
+#     },
+#     {
+#       name  = "AIRFLOW__ECS_FARGATE__LAUNCH_TYPE"
+#       value = "FARGATE"
+#     },
+#     {
+#       name  = "AIRFLOW__ECS_FARGATE__PLATFORM_VERSION"
+#       value = "LATEST"
+#     },
+#     {
+#       name  = "AIRFLOW__ECS_FARGATE__ASSIGN_PUBLIC_IP"
+#       value = "DISABLED"
+#     },
+#     {
+#       name  = "AIRFLOW__LOGGING__REMOTE_LOGGING"
+#       value = "True"
+#     },
+#     {
+#       name  = "AIRFLOW__LOGGING__REMOTE_LOG_CONN_ID"
+#       value = "aws_default"
+#     },
   ]
   # Combine static and dynamic variables
   airflow_environment_variables = concat(local.airflow_environment_static_variables, var.airflow_environment_variables)
 }
+
